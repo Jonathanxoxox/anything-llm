079433354a5ba42e732a7e681874ef2f4cf0ed35
jonathanxoxox <jonathanxoxox@users.noreply.github.com>
2025-09-02 15:25:20 +0200

    test

--- server/index.js
+++ server/index.js
@@ -135,4 +135,5 @@ app.all("*", function (_, response) {
 
 // In non-https mode we need to boot at the end since the server has not yet
 // started and is `.listen`ing.
+console.log("🔧 AnythingLLM Server - Persistence Test v1.0");
 if (!process.env.ENABLE_HTTPS) bootHTTP(app, process.env.SERVER_PORT || 3001);
